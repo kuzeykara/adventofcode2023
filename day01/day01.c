f615d6a713d003d50082b62ad8fc9691c8087dbf
omgrly <kuzeykara07@gmail.com>
2023-12-01 19:56:13 +0100

    day01 init

---
+++ day01/day01.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int part1() {
+    printf("Part 1\n");
+    
+    return 0;
+}
+
+int part2() {
+    printf("Part 2\n");
+
+    return 0;
+}
+
+
+int main(int argc, char** argv) {
+    part1();
+    part2();
+
+    return 0;
+}
