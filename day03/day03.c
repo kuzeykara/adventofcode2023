d892cde8aa6fc5276ca0768cfc4790d882a6700b
omgrly <kuzeykara07@gmail.com>
2023-12-03 14:12:44 +0100

    init day03

---
+++ day03/day03.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void part1(char *filename) {
+    FILE *fp;
+
+    if ((fp = fopen(filename, "r") == NULL)) {
+        fprintf(stderr, "Error opening file %s", filename);
+    }
+
+    printf("Part 1!\n");
+}
+
+
+void part2(char *filename) {
+    FILE *fp;
+
+    if ((fp = fopen(filename, "r") == NULL)) {
+        fprintf(stderr, "Error opening file %s", filename);
+    }
+
+    printf("Part 2!\n");
+}
+
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <1|2> <inputfile>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "1") == 0) {
+        part1(argv[2]);
+    } else if (strcmp(argv[1], "2") == 0) {
+        part2(argv[2]);
+    } else {
+        fprintf(stderr, "Usage: %s <1|2> <inputfile>\n", argv[0]);
+        return 1;
+    }
+
+    return 0;
+}
