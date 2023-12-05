d1060f64d71d06a01bdc01605399b7f0646842a5
omgrly <kuzeykara07@gmail.com>
2023-12-05 15:07:36 +0100

    day04 init

---
+++ day04/day04.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int part1(char *filename) {
+    FILE *fp;
+
+    if ((fp = fopen(filename, "r")) == NULL) {
+        fprintf(stderr, "Error opening file %s", filename);
+    }
+
+    printf("Part 1!\n");
+
+    return 0;
+}
+
+
+int part2(char *filename) {
+    FILE *fp;
+
+    if ((fp = fopen(filename, "r")) == NULL) {
+        fprintf(stderr, "Error opening file %s", filename);
+    }
+
+    printf("Part 2!\n");
+
+    return 0;
+}
+
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <1|2> <inputfile>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "1") == 0) {
+        printf("Result: %d", part1(argv[2]));
+    } else if (strcmp(argv[1], "2") == 0) {
+        printf("Result: %d", part2(argv[2])); 
+    } else {
+        fprintf(stderr, "Usage: %s <1|2> <inputfile>\n", argv[0]);
+        return 1;
+    }
+
+    return 0;
+}
