253df04fe56f355800c6148873a8dcda526f4123
omgrly <kuzeykara07@gmail.com>
2023-12-05 15:50:22 +0100

    part 1 complete

--- day04/day04.c
+++ day04/day04.c
@@ -2,17 +2,36 @@
 #include <stdio.h>
 #include <string.h>
 
+#define N 1000
+
+struct t_scratch_card {
+    int winningNumCount;
+    int winningNumbers[N/2];
+    int ownedNumCount;
+    int ownedNumbers[N/2];
+};
+
+struct t_scratch_card read_scratch(char *);
+int find_matches(struct t_scratch_card);
+int calculate_points(int);
 
 int part1(char *filename) {
     FILE *fp;
+    char currLine[N];
+    int sum=0, matches;
+    struct t_scratch_card currCard;
 
     if ((fp = fopen(filename, "r")) == NULL) {
         fprintf(stderr, "Error opening file %s", filename);
     }
 
-    printf("Part 1!\n");
+    while (fgets(currLine, N, fp)) {
+        currCard = read_scratch(currLine);
+        matches = find_matches(currCard);
+        sum += calculate_points(matches);
+    }
 
-    return 0;
+    return sum;
 }
 
 
@@ -29,6 +48,66 @@ int part2(char *filename) {
 }
 
 
+int calculate_points(int matches) {
+    int points=0;
+
+    if (matches > 0) {
+        points = 1;
+        for (int i=1; i<matches; i++) {
+            points *= 2;
+        }
+    }
+
+    return points;
+}
+
+
+int find_matches(struct t_scratch_card card) {
+    int matches = 0;
+
+    for (int i=0; i<card.ownedNumCount; i++) {
+        for (int j=0; j<card.winningNumCount; j++) {
+            if (card.ownedNumbers[i] == card.winningNumbers[j]) {
+                // MATCH!
+                matches++;
+            }
+        }
+    }
+
+    return matches;
+}
+
+
+struct t_scratch_card read_scratch(char *line) {
+    struct t_scratch_card card;
+    char *all_numbers, *buff1, *buff2, *num;
+    int winC=0, ownC=0;
+
+    all_numbers = strtok(line, ":");
+    all_numbers = strtok(NULL, ":");
+    buff1 = strtok(all_numbers, "|");
+    buff2 = strtok(NULL, "|");
+    
+    num = strtok(buff1, " ");
+    while (num != NULL) {
+        sscanf(num, "%d", &card.winningNumbers[winC]);
+        winC++;
+        num = strtok(NULL, " ");
+    }
+    card.winningNumCount = winC;
+
+    num = strtok(buff2, " ");
+    while (num != NULL) {
+        sscanf(num, "%d", &card.ownedNumbers[ownC]);
+        ownC++;
+        num = strtok(NULL, " ");
+    }
+    card.ownedNumCount = ownC;
+
+    return card;
+}
+
+
 int main(int argc, char **argv) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <1|2> <inputfile>\n", argv[0]);
@@ -36,9 +115,9 @@ int main(int argc, char **argv) {
     }
 
     if (strcmp(argv[1], "1") == 0) {
-        printf("Result: %d", part1(argv[2]));
+        printf("Result: %d\n", part1(argv[2]));
     } else if (strcmp(argv[1], "2") == 0) {
-        printf("Result: %d", part2(argv[2])); 
+        printf("Result: %d\n", part2(argv[2])); 
     } else {
         fprintf(stderr, "Usage: %s <1|2> <inputfile>\n", argv[0]);
         return 1;
