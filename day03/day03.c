5bb15242fd869f8d7390e4992e84e497ed1e0ca0
omgrly <kuzeykara07@gmail.com>
2023-12-05 15:03:37 +0100

    day03 complete

--- day03/day03.c
+++ day03/day03.c
@@ -5,10 +5,17 @@
 #define N 1000
 
 
+struct t_coords {
+    int coords[8][2];
+};
+
 int is_digit(char);
 int check_vicinity(char[][N], char[][N], int, int, int);
 int check_boundries(int, int, int, int);
 int arr_to_int(int[10], int);
+int find_gear_ratio(char[][N], int, int, int, int);
+struct t_coords coordsToCheck(int, int, int, int);
+int get_num(char[][N], int, int);
 
 int part1(char *filename) {
     FILE *fp;
@@ -54,14 +61,137 @@ int part1(char *filename) {
 }
 
 
-void part2(char *filename) {
+int part2(char *filename) {
     FILE *fp;
+    char map[N][N];
+    int sum=0, lineLength, lineC=0;
 
     if ((fp = fopen(filename, "r")) == NULL) {
         fprintf(stderr, "Error opening file %s", filename);
     }
 
-    printf("Part 2!\n");
+    // map the map to 2d array
+    while(fgets(map[lineC], N, fp) != NULL) {
+        lineC++;
+    }
+    lineLength = strlen(map[0]) - 1; // need to exclude the new line character
+
+    for (int i=0; i<lineC; i++) {
+        for (int j=0; j<=lineLength; j++) {
+            if (map[i][j] == '*') {
+                sum += find_gear_ratio(map, j, i, lineLength, lineC);
+            }
+        }
+    }
+
+    return sum;
+}
+
+
+int find_gear_ratio(char map[][N], int x, int y, int lineLength, int lineC) {
+    struct t_coords toCheck;
+    int curr_x, curr_y, num, num_length, numArr[10], start_x, full_num, k, found;
+    int fullNumArr[8];
+    
+    toCheck = coordsToCheck(x, y, lineLength, lineC);
+    found = 0;
+    for (int i=0; i<8; i++) {
+        curr_x = toCheck.coords[i][0];
+        curr_y = toCheck.coords[i][1];
+        num = get_num(map, curr_x, curr_y);
+        if ((curr_x != -1 || curr_y != -1) && num != -1) {
+            // check its left
+            num_length = 1;
+            while (check_boundries(curr_x-num_length, curr_y, lineLength, lineC) 
+                    && is_digit(map[curr_y][curr_x-num_length])) {
+                num_length++;
+            }
+            start_x = curr_x - num_length + 1;
+            // check its right
+            k=1;
+            while (check_boundries(curr_x+k, curr_y, lineLength, lineC) 
+                    && is_digit(map[curr_y][curr_x+k])) {
+                num_length++;
+                k++;
+            }
+            // extract the full number and flag any potential duplicates
+            for (int a=0; a<num_length; a++) {
+                for (int j=0; j<8; j++) {
+                    if (toCheck.coords[j][0] == start_x+a &&
+                        toCheck.coords[j][1] == curr_y) {
+                            toCheck.coords[j][0] = toCheck.coords[j][1] = -1;
+                    }
+                }
+                numArr[a] = get_num(map, start_x+a, curr_y);
+            }
+            full_num = arr_to_int(numArr, num_length);
+            fullNumArr[found] = full_num;
+            found++;
+        }
+    }
+
+    if (found == 2) {
+        return fullNumArr[0] * fullNumArr[1];
+    }
+
+    return 0;
+}
+
+
+int get_num(char map[][N], int x, int y) {
+    if (x == -1 || y == -1) {
+        return -1;
+    }
+    if (is_digit(map[y][x])) {
+        return map[y][x] - '0';
+    }
+
+    return -1;
+}
+
+
+struct t_coords coordsToCheck(int x, int y, int lineLength, int lineC) {
+    struct t_coords to_check;
+
+    for (int i=0; i<8; i++) {
+        to_check.coords[i][0] = -1;
+        to_check.coords[i][1] = -1;
+    }
+
+    if (check_boundries(x-1, y, lineLength, lineC)) { // left
+        to_check.coords[3][0] = x-1;
+        to_check.coords[3][1] = y;
+    }
+    if (check_boundries(x+1, y, lineLength, lineC)) { // right
+        to_check.coords[4][0] = x+1;
+        to_check.coords[4][1] = y;
+    }
+    if (check_boundries(x, y-1, lineLength, lineC)) { // top
+        to_check.coords[1][0] = x;
+        to_check.coords[1][1] = y-1;
+    }
+    if (check_boundries(x, y+1, lineLength, lineC)) { // bottom
+        to_check.coords[6][0] = x;
+        to_check.coords[6][1] = y+1;
+    }
+    if (check_boundries(x-1, y-1, lineLength, lineC)) { // top-left
+        to_check.coords[0][0] = x-1;
+        to_check.coords[0][1] = y-1;
+    }
+    if (check_boundries(x+1, y-1, lineLength, lineC)) { // top-right
+        to_check.coords[2][0] = x+1;
+        to_check.coords[2][1] = y-1;
+    }
+    if (check_boundries(x-1, y+1, lineLength, lineC)) { // bottom-left
+        to_check.coords[5][0] = x-1;
+        to_check.coords[5][1] = y+1;
+    }
+    if (check_boundries(x+1, y+1, lineLength, lineC)) { // bottom-right
+        to_check.coords[7][0] = x+1;
+        to_check.coords[7][1] = y+1;
+    }
+
+    return to_check;
 }
 
 
@@ -163,7 +293,7 @@ int main(int argc, char **argv) {
     if (strcmp(argv[1], "1") == 0) {
         printf("Sum: %d\n" , part1(argv[2]));
     } else if (strcmp(argv[1], "2") == 0) {
-        part2(argv[2]);
+        printf("Sum: %d\n", part2(argv[2]));
     } else {
         fprintf(stderr, "Usage: %s <1|2> <inputfile>\n", argv[0]);
         return 1;
